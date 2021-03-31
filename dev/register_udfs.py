7a827945181f38f9e5be0d4e3df3b0527df188cc
Aaron Wolen <aaron@wolen.com>
2021-03-31 09:14:56 -0500

    Script to add/update UDFs

---
+++ dev/register_udfs.py
@@ -0,0 +1,23 @@
+import tiledb.cloud
+from quokka3_udfs import vcf_annotation_example
+
+namespace = "TileDB-Inc"
+udf_name = "vcf_annotation_example"
+udf_func = vcf_annotation_example
+
+try:
+    tiledb.cloud.udf.info(namespace=namespace, name=udf_name)
+except tiledb.cloud.tiledb_cloud_error.TileDBCloudError:
+    print(f"Registering new UDF: {udf_name}")
+    tiledb.cloud.udf.register_udf(
+        func=udf_func,
+        name=udf_name,
+        namespace=namespace,
+        type="generic",
+        include_source_lines=False,
+    )
+else:
+    print(f"Update existing UDF: {udf_name}")
+    tiledb.cloud.udf.update_udf(
+        func=udf_func, name=udf_name, namespace=namespace, type="generic",
+    )
