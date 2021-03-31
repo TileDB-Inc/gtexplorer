8b641b4b6649949be163003be966d0aa41b27d47
Aaron Wolen <aaron@wolen.com>
2021-03-31 08:45:50 -0500

    Ignore dev/ when deploying

--- app.R
+++ app.R
@@ -1,4 +1,17 @@
-# rsconnect::deployApp(appName = "quokka3")
+# rsconnect::deployApp(
+#   appName = "quokka3",
+#   appFiles = c(
+#     "app.R",
+#     "DESCRIPTION",
+#     "LICENSE",
+#     "LICENSE.md",
+#     "man",
+#     "NAMESPACE",
+#     "R",
+#     "rsconnect",
+#     "shiny-quokka.Rproj"
+#   )
+# )
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 quokka3::run_app()
