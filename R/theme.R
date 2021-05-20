0cc301d0b0376a74241ce3f2c672431512ff540a
Aaron Wolen <aaron@wolen.com>
2021-05-20 12:15:00 -0500

    Update tiledb theme color

--- R/theme.R
+++ R/theme.R
@@ -2,7 +2,9 @@ gtexplorer_theme <- function() {
   bslib::bs_add_rules(
     bslib::bs_theme(
       version = 4,
-      primary = "#3190ff",
+      primary = "#4d9fff",
+      # decrease color-contrast ratio to keep text white with brand color
+      "min-contrast-ratio" = 2,
       base_font = bslib::font_google("Inter")
     ),
     rules = sass::sass_file(
